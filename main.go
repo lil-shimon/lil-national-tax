0ac719d9feda737992a6af318b1b4fbf0cc9ebfb
shimon <69175188+lil-shimon@users.noreply.github.com>
2022-07-10 01:01:07 +0900

    fix: 🐛 open .env no sush file or dir

--- main.go
+++ main.go
@@ -1,17 +1,16 @@
 package main
 
 import (
-	"github.com/joho/godotenv"
 	"github.com/line/line-bot-sdk-go/linebot"
 	"log"
 	"os"
 )
 
 func main() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal(err)
-	}
+	//err := godotenv.Load(".env")
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
 
 	bot, err := linebot.New(
 		os.Getenv("CHANNEL_SECRET"),
