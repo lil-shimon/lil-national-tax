f4fd480158f1a5a983ced62fd0501764c3410cd3
shimon <69175188+lil-shimon@users.noreply.github.com>
2022-07-10 14:35:35 +0900

    feat: 🎸 reply test

--- main.go
+++ main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-	"github.com/joho/godotenv"
 	"github.com/lil-shimon/national-tax/message"
 	"github.com/lil-shimon/national-tax/question"
 	"github.com/line/line-bot-sdk-go/linebot"
 	"log"
+	"net/http"
 	"os"
 )
 
 func main() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal(err)
-	}
+	//err := godotenv.Load(".env")
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
 
 	qs := question.GetQuestion()
 	msg := message.FmtMsg(qs)
@@ -31,8 +31,27 @@ func main() {
 		log.Fatal(err)
 	}
 
-	txt = linebot.NewTextMessage(qs.Answer)
-	if _, err := bot.BroadcastMessage(txt).Do(); err != nil {
-		log.Fatal(err)
-	}
+	//txt = linebot.NewTextMessage(qs.Answer)
+	//if _, err := bot.BroadcastMessage(txt).Do(); err != nil {
+	//	log.Fatal(err)
+	//}
+
+	http.HandleFunc("/callback", func(w http.ResponseWriter, req *http.Request) {
+		events, err := bot.ParseRequest(req)
+		if err != nil {
+			if err == linebot.ErrInvalidSignature {
+				w.WriteHeader(400)
+			} else {
+				w.WriteHeader(500)
+			}
+			return
+		}
+		for _, event := range events {
+			if event.Type == linebot.EventTypeMessage {
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(qs.Answer)).Do(); err != nil {
+					log.Print(err)
+				}
+			}
+		}
+	})
 }
